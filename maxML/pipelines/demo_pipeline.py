cae2c5c3308af0c31ca3b302becf52257122d063
maxcan7 <maxcan7@gmail.com>
2024-08-20 15:37:05 -0400

    Update demo_pipeline

--- maxML/pipelines/demo_pipeline.py
+++ maxML/pipelines/demo_pipeline.py
@@ -3,10 +3,8 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
-import yaml
 from sklearn.base import BaseEstimator
 from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 from sklearn.metrics import mean_squared_error
@@ -14,16 +12,14 @@ from sklearn.metrics import r2_score
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import OrdinalEncoder
-from sklearn.preprocessing import StandardScaler
 
+from maxML.config_schemas import load_config
 from maxML.config_schemas import PipelineConfig
+from maxML.preprocessors import ColumnTransformerPreprocessor
 
 
 """
-This demo_pipeline is a more conventional approach to running an sklearn model
-pipeline end-to-end.
+demo_pipeline for running an sklearn model pipeline end-to-end using maxML.
 """
 
 
@@ -77,46 +73,6 @@ def create_model_pipeline(
     return Pipeline([("preprocessor", preprocessor), ("model", model)])
 
 
-def create_preprocessor() -> ColumnTransformer:
-    """TODO: Configure"""
-    numeric_pipeline = Pipeline(
-        [
-            ("imputer", SimpleImputer(strategy="median")),
-            ("scaler", StandardScaler()),
-        ]
-    )
-    numeric_columns = ["Age", "Income", "Years_of_Experience"]
-    numeric = ("numeric", numeric_pipeline, numeric_columns)
-
-    nominal_pipeline = Pipeline(
-        [
-            ("imputer", SimpleImputer(strategy="most_frequent")),
-            ("onehot", OneHotEncoder(handle_unknown="ignore")),
-        ]
-    )
-    nominal_columns = ["Gender", "City"]
-    nominal = ("nominal", nominal_pipeline, nominal_columns)
-
-    ed_categories = ["High School", "Bachelor's", "Master's" "PhD"]
-    ordinal_pipeline = Pipeline(
-        [
-            ("imputer", SimpleImputer(strategy="most_frequent")),
-            (
-                "ordinal",
-                # NOTE: Unkown Value is 4 bc HS -> PhD == 1 -> 3.
-                OrdinalEncoder(
-                    categories=[ed_categories],
-                    handle_unknown="use_encoded_value",
-                    unknown_value=4,
-                ),
-            ),
-        ]
-    )
-    ordinal = ("ordinal", ordinal_pipeline, ["Education"])
-
-    return ColumnTransformer(transformers=[numeric, nominal, ordinal])
-
-
 def load_data(input_path: str) -> pd.DataFrame:
     """Load data from input_path as pandas DataFrame."""
     return pd.read_csv(Path.cwd() / input_path)
@@ -126,11 +82,10 @@ def main(pipeline_config_path: str) -> None:
     """
     Run sklearn pipelines on dataset with preprocessing steps.
     """
-    pipeline_config = PipelineConfig(
-        **yaml.safe_load(open(Path.cwd() / pipeline_config_path))
-    )
+    pipeline_config = load_config(PipelineConfig, pipeline_config_path)
     df = load_data(pipeline_config.input_path)
-    preprocessor = create_preprocessor()
+    # TODO: Abstract this to handle any Preprocessor.
+    preprocessor = ColumnTransformerPreprocessor.compose(pipeline_config)
 
     pipeline = create_model_pipeline(
         model=pipeline_config.sklearn_model, preprocessor=preprocessor
