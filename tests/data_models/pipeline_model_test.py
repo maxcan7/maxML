3f2ab9fcb16c1447af6542cd77f26f63d2be9a7c
maxcan7 <maxcan7@gmail.com>
2024-08-20 11:03:06 -0400

    Add linear_model test; Update pre-commit

--- tests/data_models/pipeline_model_test.py
+++ tests/data_models/pipeline_model_test.py
@@ -1,12 +1,30 @@
-import typing as T
+from pathlib import Path
 
+import pytest
+import yaml
 from sklearn.base import BaseEstimator
+from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
 
 from maxML.data_models.pipeline_model import PipelineModel
 
 
-def test_retrieve_model_type(logistic_model_config: dict[str, T.Any]):
-    pipeline_model = PipelineModel(**logistic_model_config)
+@pytest.mark.parametrize(
+    "model_config_path, model_estimator",
+    [
+        pytest.param(
+            "tests/test_configs/logistic_model.yaml",
+            LogisticRegression,
+            id="logistic_model",
+        ),
+        pytest.param(
+            "tests/test_configs/linear_model.yaml", LinearRegression, id="linear_model"
+        ),
+    ],
+)
+def test_retrieve_model_type(model_config_path: str, model_estimator: BaseEstimator):
+    pipeline_model = PipelineModel(
+        **yaml.safe_load(open(Path.cwd() / model_config_path))
+    )
     assert isinstance(pipeline_model.sklearn_model, BaseEstimator)
-    assert isinstance(pipeline_model.sklearn_model, LogisticRegression)
+    assert isinstance(pipeline_model.sklearn_model, model_estimator)
