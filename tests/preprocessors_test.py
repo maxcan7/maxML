62a68af61d274b39693eee008af5a513156ca4e1
maxcan7 <maxcan7@gmail.com>
2024-10-07 13:02:39 -0400

    Update for load_config change

--- tests/preprocessors_test.py
+++ tests/preprocessors_test.py
@@ -36,7 +36,7 @@ def test_get_preprocessor_fn(
     Test that the get_preprocessor function can load the correct Preprocessors given the
     PipelineConfig params.
     """
-    pipeline_config = load_config(PipelineConfig, pipeline_config_path)
+    pipeline_config: PipelineConfig = load_config(pipeline_config_path)
     for i in range(len(pipeline_config.preprocessors)):
         preprocessor = get_preprocessor_fn(pipeline_config.preprocessors[i])
         assert preprocessor == expected_preprocessors[i]
@@ -56,7 +56,7 @@ def test_do_preprocessing(pipeline_config_path: str):
     Test that if both the PreprocessorConfig preprocessor and pipelines fields are None,
     do_preprocessing returns False.
     """
-    pipeline_config = load_config(PipelineConfig, pipeline_config_path)
+    pipeline_config: PipelineConfig = load_config(pipeline_config_path)
     assert not do_preprocessing(pipeline_config.preprocessors)
 
 
@@ -74,7 +74,7 @@ def test_get_preprocessor_fn_invalid(pipeline_config_path: str):
     Test that the get_preprocessor function will return a KeyError if the preprocessor
     or pipelines fields are None.
     """
-    pipeline_config = load_config(PipelineConfig, pipeline_config_path)
+    pipeline_config: PipelineConfig = load_config(pipeline_config_path)
     with pytest.raises(KeyError):
         get_preprocessor_fn(pipeline_config.preprocessors[0])
 
@@ -96,7 +96,7 @@ def test_ColumnTransformerPreprocessor_compose(
     Test that the ColumnTransformerPreprocessor can compose the PipelineConfig into a
     ColumnTransformer.
     """
-    pipeline_config = load_config(PipelineConfig, pipeline_config_path)
+    pipeline_config: PipelineConfig = load_config(pipeline_config_path)
     preprocessor = ColumnTransformerPreprocessor.compose(
         pipeline_config.preprocessors[0]
     )
@@ -122,7 +122,7 @@ def test_FeatureUnionPreprocessor_compose(
     Test that the FeatureUnionPreprocessor can compose the PipelineConfig into a
     FeatureUnion.
     """
-    pipeline_config = load_config(PipelineConfig, pipeline_config_path)
+    pipeline_config: PipelineConfig = load_config(pipeline_config_path)
     preprocessor = FeatureUnionPreprocessor.compose(pipeline_config.preprocessors[1])
     # TODO: Figure out a way to compare these two objects effectively.
     assert preprocessor
@@ -152,7 +152,7 @@ def test_compose_preprocessor(
     Test that the compose_preprocessor function can properly compose multiple
     preprocessors into one.
     """
-    pipeline_configs = load_config(PipelineConfig, pipeline_config_path)
+    pipeline_configs: PipelineConfig = load_config(pipeline_config_path)
     preprocessor = compose_preprocessor(pipeline_configs.preprocessors)
     # TODO: Figure out a way to compare these two objects effectively.
     assert preprocessor
